95efabb79a8ad7c74a771e071940b12e9fdd87e9
djb212 <djbaker@cantab.net>
2022-02-06 21:43:17 +0000

    Initial commit

---
+++ Projector.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Test
+{
+    public class Projector
+    {
+        #region Properties
+        public Inputs Inputs;
+        #endregion
+
+        #region Constructor
+        public Projector(Inputs inputs)
+        {
+            Inputs = inputs;
+        }
+        #endregion
+
+        #region Public Methods
+        public double[] GetInflatedCost()
+        {
+            double[] projections = new double[Inputs.Time];
+            double factor = (Inputs.Inflation + 100) / 100;
+
+            for (int i = 0; i < Inputs.Time; i++)
+            {
+                projections[i] = Inputs.Cost * GetProjectionFactor(factor, i);
+            }
+
+            return projections;
+        }
+        
+        public double[] GetInflatedCostWuthDecrement()
+        {
+            double[] projections = GetInflatedCost();
+
+            double[] projectionsWithDecrement = new double[Inputs.Time];
+            double factor = (100 + Inputs._annualChangeInYearlyPayments)/100;
+
+            for (int i = 0; i < projections.Length; i++)
+            {
+                projectionsWithDecrement[i] = projections[i] * Math.Pow(factor, i + 1);
+            }
+
+            return projectionsWithDecrement;
+        }
+
+        public void UpdateInflatedCostWithDecrement(double[] salaryProjectionsWithoutDecrement)
+        {
+            for (int i = 0; i < Inputs.Time; i++)
+            {
+                salaryProjectionsWithoutDecrement[i] *= Inputs.GetAvgPercentOfAnnualChangeInPayments(i);
+            }
+        }
+        #endregion
+
+        #region Private Methods
+        private static double GetProjectionFactor(double factor, int cashFlowYear)
+        {
+            return (Math.Pow(factor, cashFlowYear));
+        }
+        #endregion
+    }
+}
